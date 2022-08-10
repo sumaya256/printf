ce7792582e23494804798c98fede48dccaba257e
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-08-10 21:13:15 +0300

    Update print_rev.c

--- print_rev.c
+++ print_rev.c
@@ -1,24 +1,18 @@
 #include "main.h"
-
 /**
- * print_rev - prints a string in reverse
- * @l: argument from _printf
- * Return: length of the printed string
- */
-
+* print_rev - prints a string in reverse
+* @l: argument from _printf
+* Return: length of the printed string
+*/
 int print_rev(va_list l)
 {
-	int i = 0, j;
-	char *s = va_arg(l, char *);
-
+int i = 0, j;
+char *s = va_arg(l, char *);
 	if (!s)
-		s = "(null)";
-
+	s = "(null)";
 	while (s[i])
-		i++;
-
+	i++;
 	for (j = i - 1; j >= 0; j--)
-		_putchar(s[j]);
-
+	_putchar(s[j]);
 	return (i);
 }
